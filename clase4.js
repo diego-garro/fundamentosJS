5b205bed8f8a1825ebd0b0a4090b6edafcc780a8
diegogarromolina <diego.garromolina@ucr.ac.cr>
2019-10-12 23:44:32 -0600

    Clases 4 y 5 de la sección 2

---
+++ clase4.js
@@ -0,0 +1,11 @@
+
+var nombre = 'Diego', edad = 33
+
+function imprimirEdad(n, e) {
+	console.log(`${n} tiene ${e} años!`)
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad('Vicky', 28)
+imprimirEdad('Eric', 24)
+imprimirEdad('Darío', 35)
---
+++ clase5.js
@@ -0,0 +1,11 @@
+
+var nombre = 'Diego'
+
+function imprimirNombreEnMAyusculas(nombre) {
+	nombre = nombre.toUpperCase();
+	console.log(nombre);
+}
+
+imprimirNombreEnMAyusculas(nombre)
+
+console.log(nombre)
