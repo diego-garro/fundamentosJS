c3ab6925858ee9a160a10f5df2a90ee721b83125
diegogarromolina <diego.garromolina@ucr.ac.cr>
2019-11-01 01:49:14 -0600

    Terminado capitulo 3.2

---
+++ clase11.js
@@ -0,0 +1,29 @@
+var diego = {
+    nombre: 'Diego',
+    edad: 33,
+    drone: false,
+    guitarrista: false,
+    programador: true
+}
+
+var juan = {
+    nombre: 'Juan',
+    edad: 17,
+    drone: false,
+    guitarrista: true,
+    programador: false
+}
+
+const MAYORIA_DE_EDAD = 18
+
+function esMayorDeEdad(persona) {
+    return persona.edad >= MAYORIA_DE_EDAD
+}
+
+function imprimirSiEsMayorDeEdad(persona) {
+    if (esMayorDeEdad(persona)) {
+        console.log(`${persona.nombre} es mayor de edad.`)
+    } else {
+        console.log(`${persona.nombre} es menor de edad.`)
+    }
+}
