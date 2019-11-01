cb0ccfbff6d0a91ad43c17179e98fc60b12b7de6
diegogarromolina <diego.garromolina@ucr.ac.cr>
2019-10-31 23:12:06 -0600

    Capitulo 2.7 terminado

--- clase6.js
+++ clase6.js
@@ -11,12 +11,23 @@ var dario = {
 	edad: 30
 }
 
-function imprimirNombreEnMAyusculas({nombre}) {
+function imprimirNombreEnMAyusculas(persona) {
+	// var bombre = persona.nombre
+	var { nombre } = persona
 	console.log(nombre.toUpperCase());
+}
+
+function imprimirNombreYEdad(persona) {
+	// Hola, soy Diego y tengo 30 años
+	var { nombre } = persona
+	var { edad } = persona
+	console.log(`Hola, soy ${nombre} y tengo ${edad} años.`)
 }
 
 imprimirNombreEnMAyusculas(diego)
 imprimirNombreEnMAyusculas(dario)
-imprimirNombreEnMAyusculas({nombre: 'Pepito'})
-imprimirNombreEnMAyusculas()
+//imprimirNombreEnMAyusculas({nombre: 'Pepito'})
+//imprimirNombreEnMAyusculas()
+imprimirNombreYEdad(diego)
+imprimirNombreYEdad(dario)
 
