418c49101c102549543d75c5fcff9c16d48a55c6
diegogarromolina <diego.garromolina@ucr.ac.cr>
2019-11-03 22:46:25 -0600

    Capitulo 3.7 terminado

--- clase16.js
+++ clase16.js
@@ -1,13 +1,19 @@
-var contador = 0
+var signo = prompt('Cuál es tu signo?')
 
-const llueve = () => Math.random() < 0.25
-
-do {
-    contador++
-} while (!llueve())
-
-if (contador == 1) {
-    console.log(`Fui a ver si llovía ${contador} vez.`)
-} else {
-    console.log(`Fui a ver si llovía ${contador} veces.`)
+switch (signo) {
+    case 'acuario':
+        console.log('Romperás viejas ataduras sentimentales y te proyectarás al futuro con seguridad.')
+        break
+    case 'tauro':
+        console.log('Comienza para ti un período de amor y pasión.')
+        break
+    case 'geminis':
+        console.log('Un lunes para explorar algo nuevo en tu intimidad.')
+        break
+    case 'cancer':
+        console.log('Una noticia inesperada alegrará el día y toda la semana.')
+        break
+    default:
+        console.log(`${signo} no es un signo del Zodiaco!`)
+        break
 }
