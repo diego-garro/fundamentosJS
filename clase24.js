5658cd753970b0fdb2b6d90585dc5c3391c2e5c5
diegogarromolina <diego.garromolina@ucr.ac.cr>
2019-11-09 23:00:45 -0600

    Clase 5.4 terminada

--- clase24.js
+++ clase24.js
@@ -1,3 +1,11 @@
+function heredaDe(prototipoHijo, prototipoPadre) {
+    var fn = function () {}
+
+    fn.prototype = prototipoPadre.prototype
+    prototipoHijo.prototype = new fn
+    prototipoHijo.prototype.constructor = prototipoHijo
+}
+
 function Persona(nombre, apellido, altura) {
     this.nombre = nombre
     this.apellido = apellido
@@ -12,10 +20,19 @@ Persona.prototype.soyAlto = function () {
     return this.altura > 1.8
 }
 
-var diego = new Persona('Diego', 'Garro', 1.78)
-var ericka = new Persona('Ericka', 'Luna', 1.65)
-var arturo = new Persona('Arturo', 'Martinez', 1.82)
+function Desarrollador(nombre, apellido) {
+    this.nombre = nombre
+    this.apellido = apellido
+}
 
-diego.saludar()
-ericka.saludar()
-arturo.saludar()
+heredaDe(Desarrollador, Persona)
+
+Desarrollador.prototype.saludar = function () {
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador!`)
+}
+
+//heredaDe(Desarrollador, Persona)
+
+//var diego = new Persona('Diego', 'Garro', 1.78)
+//var ericka = new Persona('Ericka', 'Luna', 1.65)
+//var arturo = new Persona('Arturo', 'Martinez', 1.82)
